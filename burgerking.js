5be70eb2690cb236f76c1b8dc229c9ca22bfad06
Munkdigital <tobiasmunk21@gmail.com>
2022-08-30 14:26:30 +0200

    update

---
+++ burgerking.js
@@ -0,0 +1,18 @@
+var picss_i = 0;
+var picss_array = ["yo","lol","hej","wasup"];
+var picss_elem;
+function picssNext() {
+    picss_i++;
+    picss_elem.style.opacity = 0;
+    if(picss_i > (picss_array.length - 1)) {
+        picss_i = 0;
+    }
+    setTimeout('picssSlide()',1000);
+}
+function picssSlide () {
+    picss_elem.innerHTML = picss_array[picss_i];
+    picss_elem.style.opacity = 1;
+    setTimeout('picssNext()',2000);
+}
+
+picss_elem = document.getElementById("picss"); picssSlide();
