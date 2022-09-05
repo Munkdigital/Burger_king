df4f78b16af6b3ecc215a0d3728fb0f0cf228a23
Munkdigital <tobiasmunk21@gmail.com>
2022-09-05 23:16:16 +0200

    for loop update

--- burgerking1.js
+++ burgerking1.js
@@ -59,14 +59,20 @@ var picss_i = 0;
 var picss_main = document.getElementById('picss')
 var picss_array = ["bk_slideshow/burger1.png","bk_slideshow/burger2.png","bk_slideshow/burger3.png","bk_slideshow/burger4.png"];
 var picss_elem;
+var picss_error = ["Error", "br", "Reload page"];
         
         function picssNext() {
             picss_main.setAttribute("src",picss_array[picss_i])
             picss_i++;
             picss_elem.style.opacity = 0;
-            if(picss_i > (picss_array.length - 1)) {
+                if(picss_i > (picss_array.length - 1)) {
                 picss_i = 0;
-            }
+                }
+                if(picss_i > (picss_array.length + 1)){
+                    for (var picss_i2 = 0; picss_i2 < picss_error.length; picss_i2++){
+                        console.log(picss_error[picss_i2]);
+                    }
+                }
             setTimeout('picssSlide()',1500);
         }
         function picssSlide () {
